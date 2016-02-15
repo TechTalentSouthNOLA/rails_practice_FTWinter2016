44f4966b7d98cd22664384e3f0a8dd71077b1817
Sia <siakaramalegos@gmail.com>
2016-02-15 11:54:05 -0600

    Adds new and create actions for dogs

--- doggie_daycare/app/controllers/dogs_controller.rb
+++ doggie_daycare/app/controllers/dogs_controller.rb
@@ -2,4 +2,24 @@ class DogsController < ApplicationController
   def index
     @dogs = Dog.all
   end
+
+  def new
+    @dog = Dog.new
+  end
+
+  def create
+    @dog = Dog.new(dog_params)
+    if @dog.save
+      redirect_to dogs_url, notice: "Dog saved!"
+    else
+      flash.now[:notice] = "Could not save dog. :("
+      render :new
+    end
+  end
+
+  private
+
+  def dog_params
+    params.require(:dog).permit(:name, :breed, :age)
+  end
 end
--- doggie_daycare/config/routes.rb
+++ doggie_daycare/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :dogs, only: [:index]
+  resources :dogs, only: [:index, :new, :create]
 
   get 'pages/index'
 
