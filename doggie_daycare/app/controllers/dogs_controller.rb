b849d7dd9dab9dd483741c9c041d7958d2bfc48b
Sia <siakaramalegos@gmail.com>
2016-02-16 09:38:05 -0600

    Adds edit and update for dogs

--- doggie_daycare/app/controllers/dogs_controller.rb
+++ doggie_daycare/app/controllers/dogs_controller.rb
@@ -14,11 +14,28 @@ class DogsController < ApplicationController
 
   def create
     @dog = Dog.new(dog_params)
+
     if @dog.save
       redirect_to dogs_url, notice: "Dog saved!"
     else
       flash.now[:notice] = "Could not save dog. :("
       render :new
+    end
+  end
+
+  def edit
+    @dog = Dog.find(params[:id])
+  end
+
+  def update
+    @dog = Dog.find(params[:id])
+
+    if @dog.update(dog_params)
+      flash[:notice] = "Dog updated!"
+      redirect_to dog_url(@dog)
+    else
+      flash.now[:notice] = "Could not update dog :("
+      render :edit
     end
   end
 
